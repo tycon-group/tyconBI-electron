fdfcd0d40191bba868acd816ab73eac000aa8ec6
Leoforpeng <leo.peng@shtycon.com>
2020-08-24 11:14:05 +0800

    新建KPI页面，做好了上一月下一月样式

---
+++ src/renderer/components/KPIAnalysis.vue
@@ -0,0 +1,88 @@
+<template>
+  <a-layout id="components-layout-demo-custom-trigger" class="layout">
+    <Menus />
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <div class="header-total">
+          <SearchInput />
+          <MonthPage />
+        </div>
+
+      </a-layout-header>
+      <div class="mid_content">
+        <div class="meun_mid">
+          <ContentLeft />
+        </div>
+
+        <a-layout-content class="content">
+          <ContentsUp />
+
+        </a-layout-content>
+
+        <ContentsDown />
+
+      </div>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+import Menus from './path/Menus.vue';
+import ContentLeft from './path/ContentLeft.vue';
+import ContentsUp from './path/ContentsUp';
+import ContentsDown from './path/ContentsDown';
+import SearchInput from './path/SearchInput';
+import MonthPage from './path/MonthPage';
+
+export default {
+  name: 'KPIAnalysis',
+  components: {
+    MonthPage,
+    ContentsDown,
+    ContentsUp,
+    Menus,
+    ContentLeft,
+    SearchInput,
+  },
+
+};
+</script>
+
+<style>
+
+.layout {
+    width: 100%;
+    height: 100%;
+  }
+
+.mid_content {
+  width: 100%;
+  height: 100%;
+}
+
+.meun_mid {
+  width: 30%;
+  min-height: 90%;
+  float: left;
+  background: #ffffff;
+  margin: 24px 16px;
+  padding: 24px;
+}
+
+.content {
+  width: 60%;
+  margin: 24px 16px;
+  padding: 24px;
+  background: #ffffff;
+  min-Height: 40%;
+  float: left;
+}
+.mark_submit {
+  width: 60%;
+  min-Height: 40%;
+  margin: 20px 16px;
+  padding: 24px;
+  background: #ffffff;
+  float: left;
+}
+</style>
---
+++ src/renderer/components/path/MonthPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="monthpage">
+    <a-button class="uptime" type="link"><a-icon type="left" /></a-button>
+      <span>2020年7月</span>
+    <a-button class="downtime" type="link"><a-icon type="right" /></a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MonthPage',
+};
+</script>
+
+<style>
+
+</style>
