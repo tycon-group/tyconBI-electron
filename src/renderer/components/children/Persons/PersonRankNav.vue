37fb072fa58fba8f589b2e5408731ea5d997611b
Leoforpeng <leo.peng@shtycon.com>
2020-09-01 15:33:42 +0800

    删除个人页面的工作能力中的tab多余的div

--- src/renderer/components/children/Persons/PersonRankNav.vue
+++ src/renderer/components/children/Persons/PersonRankNav.vue
@@ -1,25 +1,23 @@
 <template>
   <div class="personListNav">
-    <div class="groupListNav">
-      <a-tabs default-active-key="work" @change="callback">
-        <a-tab-pane key="work" tab="工作能力">
-          <WorkRank />
-        </a-tab-pane>
-        <a-tab-pane key="attitude" tab="工作态度">
-          <AttitudeRank />
-        </a-tab-pane>
+    <a-tabs default-active-key="work" @change="callback">
+      <a-tab-pane key="work" tab="工作能力">
+        <WorkRank />
+      </a-tab-pane>
+      <a-tab-pane key="attitude" tab="工作态度">
+        <AttitudeRank />
+      </a-tab-pane>
 
-        <a-tab-pane key="potential" tab="发展潜力">
-          <PotentialRank />
-        </a-tab-pane>
-        <a-tab-pane key="contribution" tab="突出贡献">
-          <ContributionRank />
-        </a-tab-pane>
-        <a-tab-pane key="law" tab="遵纪守法">
-          <LawRank />
-        </a-tab-pane>
-      </a-tabs>
-    </div>
+      <a-tab-pane key="potential" tab="发展潜力">
+        <PotentialRank />
+      </a-tab-pane>
+      <a-tab-pane key="contribution" tab="突出贡献">
+        <ContributionRank />
+      </a-tab-pane>
+      <a-tab-pane key="law" tab="遵纪守法">
+        <LawRank />
+      </a-tab-pane>
+    </a-tabs>
   </div>
 </template>
 
