e22872d5454fb59ee3481db22e7bd550ae29d77c
Leoforpeng <leo.peng@shtycon.com>
2020-09-04 17:51:44 +0800

    修改左下角用户的位置

--- src/renderer/components/path/Menus.vue
+++ src/renderer/components/path/Menus.vue
@@ -26,7 +26,7 @@
       </a-menu-item>
     </a-menu>
     <div class="title_footer">
-      <a-avatar :size="64" src="https://tyconstore-1302115199.cos.ap-shanghai.myqcloud.com/leo.jpg" alt="加载失败"/>
+      <div class="img_photo"><a-avatar :size="32" src="https://tyconstore-1302115199.cos.ap-shanghai.myqcloud.com/leo.jpg" alt="加载失败"/></div>
       <div class="user_name">摆鱼</div>
       <div class="company_name">上海太江信息科技有限公司</div>
     </div>
@@ -59,24 +59,33 @@ export default {
 }
 
 .title_footer {
-  height: 80px;
-  margin: 25px;
+  height: 55px;
+  margin-left: 12px;
+  margin-bottom: 1px;
   font-size: 12px;
   color: #888888;
   position:fixed;
   bottom:0;
 }
 
+.img_photo {
+  width: 35px;
+  height: auto;
+  margin-left: 1px;
+  float: left;
+  margin-top: 7px;
+}
+
 .user_name {
-  width: 100%;
+  width: 180px;
   height: 10px;
-  color: azure;
-  font-size: 15px;
+  font-size: 12px;
+  margin-left: 12px;
 }
 
 .company_name {
-  width: 100%;
+  width: 200px;
+  font-size: 12px;
   margin-top: 10px;
-  height: 10px;
 }
 </style>
