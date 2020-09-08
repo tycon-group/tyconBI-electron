069195f92863a289df1cb3aab0f5fc38daa5db3b
Leoforpeng <leo.peng@shtycon.com>
2020-09-08 15:41:54 +0800

    接口判断登录成功

--- src/renderer/components/Login.vue
+++ src/renderer/components/Login.vue
@@ -1,60 +1,52 @@
 <template>
-  <a-form
-      id="components-form-demo-normal-login"
-      :form="form"
-      class="login-form"
-      @submit="handleSubmit"
-      :model="forms"
-  >
-    <a-form-item>
-      <a-input
-          v-decorator="[
-          'userName',
-          { rules: [{ required: true, message: 'Please input your username!' }], initialValue: forms.userName, },
-        ]"
-          placeholder="Username"
-          this.form.setFieldsValue='forms.userName'
-      >
-        <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
-      </a-input>
-    </a-form-item>
-    <a-form-item>
-      <a-input
-          v-decorator="[
-          'password',
-          { rules: [{ required: true, message: 'Please input your Password!' }], initialValue: forms.password, },
-        ]"
-          type="password"
-          placeholder="Password"
-          this.form.setFieldsValue='forms.password'
-      >
-        <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
-      </a-input>
-    </a-form-item>
-    <a-form-item>
-      <a-checkbox
-          v-decorator="[
-          'remember',
-          {
-            valuePropName: 'checked',
-            initialValue: true,
-          },
-        ]"
-      >
-        Remember me
-      </a-checkbox>
-      <a class="login-form-forgot" href="#">
-        Forgot password
-      </a>
-      <a-button type="primary" html-type="submit" class="login-form-button">
-        Log in
-      </a-button>
-      Or
-      <a href="#">
-        register now!
-      </a>
-    </a-form-item>
-  </a-form>
+  <div class="login-form">
+    <div>
+      <img class="logo_img" src="../assets/tycon.png" />
+    </div>
+    <a-form-model
+        id="components-form-demo-normal-login"
+        @submit="handleSubmit"
+        :model="forms"
+    >
+      <a-form-model-item>
+        <a-input
+            placeholder="Username"
+            v-model='forms.userName'
+        >
+          <a-icon slot="prefix" type="user" style="color: rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-input
+            type="password"
+            placeholder="Password"
+            v-model='forms.password'
+        >
+          <a-icon slot="prefix" type="lock" style="color: rgba(0,0,0,.25)" />
+        </a-input>
+      </a-form-model-item>
+      <a-form-model-item>
+        <a-checkbox
+            v-decorator="[
+            'remember',
+            {
+              valuePropName: 'checked',
+              initialValue: true,
+            },
+          ]"
+        >
+          Remember me
+        </a-checkbox>
+        <a class="login-form-forgot" href="#">
+          Forgot password
+        </a>
+        <a-button type="primary" html-type="submit" class="login-form-button"
+                  :disabled="forms.userName === '' || forms.password === ''">
+          Log in
+        </a-button>
+      </a-form-model-item>
+    </a-form-model>
+  </div>
 </template>
 
 <script>
@@ -67,36 +59,48 @@ export default {
       },
     };
   },
-  beforeCreate() {
-    this.form = this.$form.createForm(this, { name: 'admin', password: '123456' });
-  },
   created() {
-    this.$http.get('http://api.tianapi.com/txapi/tiangou/index?key=291426a3508a7d01328183ea901c4bd2')
+    this.$http.get('http://tyconcps.cn:8888/users')
       .then((res) => {
         console.log(res);
       })
     // eslint-disable-next-line no-unused-vars
       .catch((res) => {
-
       });
   },
   methods: {
-    handleSubmit(e) {
-      e.preventDefault();
+    handleSubmit() {
+      const param = new URLSearchParams();
+      param.append('username', this.forms.userName);
+      param.append('password', this.forms.password);
+      const instance = this.$http.create({ headers: { 'content-type': 'application/x-www-form-urlencoded' } });
+      instance.post('http://tyconcps.cn:8888/auth-token/', param).then((response) => {
+        console.log(response);
+        this.$message.success('登录成功');
+      }).catch((error) => {
+        console.log(error);
+        this.$message.error('账号或密码错误');
+      });
     },
   },
 };
 </script>
 
 <style scoped>
-.login-form {
-  max-width: 300px;
-  margin:0 auto;
+.logo_img {
+  width: 160px;
+  padding: 14px;
+  margin-top: -20px;
 }
-#components-form-demo-normal-login .login-form-forgot {
+.login-form {
+  width: 300px;
+  margin:50px auto;
+
+}
+.login-form-forgot {
   float: right;
 }
-#components-form-demo-normal-login .login-form-button {
+.login-form-button {
   width: 100%;
 }
 </style>
