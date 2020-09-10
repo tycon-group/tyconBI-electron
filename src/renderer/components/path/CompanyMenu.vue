734b67e819f4af5d2a8ff1c9020100ec056f9be2
Leoforpeng <leo.peng@shtycon.com>
2020-09-10 10:23:40 +0800

    kip分析页面中，增加部门这一级

--- src/renderer/components/path/CompanyMenu.vue
+++ src/renderer/components/path/CompanyMenu.vue
@@ -11,27 +11,32 @@
         <span slot="title"><router-link to="/analysis/analysisGroup" tag="span">上海太江集团有限公司</router-link></span>
         <a-sub-menu key="sub2">
           <span slot="title"><router-link to="/analysis/analysisGroup" tag="span">上海太江建设工程有限公司</router-link></span>
-
-          <a-menu-item key="1">
-            <router-link to="/analysis/analysisPerson">
-              <span>张三</span>
-            </router-link>
-          </a-menu-item>
-
+          <a-sub-menu key="sub2_1">
+            <span slot="title"><router-link to="/analysis/analysisGroup" tag="span">项目部</router-link></span>
+            <a-menu-item key="1">
+              <router-link to="/analysis/analysisPerson">
+                <span>张三</span>
+              </router-link>
+            </a-menu-item>
+          </a-sub-menu>
         </a-sub-menu>
 
         <a-sub-menu key="sub3">
           <span slot="title"><router-link to="/analysis/analysisGroup" tag="span">上海太江信息科技有限公司</router-link></span>
-          <a-menu-item key="2">
-            <router-link to="/analysis/analysisPerson">
-              <span>李四</span>
-            </router-link>
-          </a-menu-item>
-          <a-menu-item key="3">
-            <router-link to="/analysis/analysisPerson">
-              <span>王二麻子</span>
-            </router-link>
-          </a-menu-item>
+          <a-sub-menu key="sub2_1">
+            <span slot="title"><router-link to="/analysis/analysisGroup" tag="span">研发部</router-link></span>
+              <a-menu-item key="2">
+              <router-link to="/analysis/analysisPerson">
+                <span>李四</span>
+              </router-link>
+            </a-menu-item>
+
+            <a-menu-item key="3">
+              <router-link to="/analysis/analysisPerson">
+                <span>王二麻子</span>
+              </router-link>
+            </a-menu-item>
+          </a-sub-menu>
         </a-sub-menu>
 
         <a-sub-menu key="sub4">
