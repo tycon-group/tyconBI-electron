8407db08fed171c93493b199568811e8caba53f0
Leoforpeng <leo.peng@shtycon.com>
2020-10-16 13:09:29 +0800

    提交了头像值

--- src/renderer/components/path/PersonInfo.vue
+++ src/renderer/components/path/PersonInfo.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="personInfo">
     <div class="personImages">
-      <img class="personImage" src="../../assets/leo.png"  alt="头像"/>
+      <img class="personImage" v-bind:src="this.avatar"  alt="暂无头像"/>
     </div>
 
     <div class="manInfo">
       <div class="row">
         <div class="row_left">姓名：{{ zhName }}  {{ enName }}</div>
-        <div class="row_right">性别：男</div>
+        <div class="row_right">性别：{{ gender }}</div>
       </div>
       <div class="row">
         <div class="row_left">职位：上海太江信息科技有限公司/工程师</div>
-        <div class="row_right">学历：本科</div>
+        <div class="row_right">学历：{{ education }}</div>
       </div>
       <div class="row">
-        <div class="row_left">出生年月：1990.09.09</div>
-        <div class="row_right">入职日期：2020.01.01</div>
+        <div class="row_left">出生年月：{{ dateOfBirth }}</div>
+        <div class="row_right">入职日期：{{ dateOfHire }}</div>
       </div>
       <div class="row">
         <div class="row_left">连续工龄：5年</div>
@@ -37,13 +37,14 @@ export default {
       itemData: [],
       zhName: '',
       enName: '',
-      sex: '',
+      gender: '',
       zhPosition: '',
       education: '',
-      birthday: '',
+      dateOfBirth: '',
       dateOfHire: '',
-      timeForWork: '',
-      timeInCompany: '',
+      yearForWork: '',
+      yearInCompany: '',
+      avatar: '',
     };
   },
   created() {
@@ -58,7 +59,17 @@ export default {
           this.itemData = res.data;
           this.zhName = this.itemData.name;
           this.enName = this.itemData.enName;
-          console.log(this.zhName);
+          // 判断性别
+          const sex = this.itemData.gender;
+          if (sex === 'M') {
+            this.gender = '男';
+          } else {
+            this.gender = '女';
+          }
+          this.education = this.itemData.education;
+          this.dateOfBirth = this.itemData.dateOfBirth;
+          this.dateOfHire = this.itemData.dateOfHire;
+          this.avatar = this.itemData.avatar;
         })
         .catch((error) => {
           console.log(error);
