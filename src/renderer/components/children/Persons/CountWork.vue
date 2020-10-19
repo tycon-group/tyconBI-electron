de759503798aedd7c9f8eb42d4cff52abbd4308c
Leoforpeng <leo.peng@shtycon.com>
2020-10-19 15:31:37 +0800

    添加补写数，评分数区分人事和直属，优化css

--- src/renderer/components/children/Persons/CountWork.vue
+++ src/renderer/components/children/Persons/CountWork.vue
@@ -5,22 +5,28 @@
       <br/>
       <span class="partValue">18</span>
     </div>
-    <div class="col2">
+    <div class="col1">
       <span class="partName">日志评论数 ></span>
       <br/>
       <span class="partValue">18</span>
     </div>
-    <div class="col3">
+    <div class="col1">
       <span class="partName">日志评分数 ></span>
       <br/>
-      <span class="partValue">15</span>
+      <span class="partName">直属：</span><span class="partValue">15</span>
+      <span class="partName">人事：</span><span class="partValue">10</span>
     </div>
-    <div class="col4">
+    <div class="col2">
+      <span class="partName">日志补写数 ></span>
+      <br/>
+      <span class="partValue">7</span>
+    </div>
+    <div class="col2">
       <span class="partName">高评数 ></span>
       <br/>
       <span class="partValue">7</span>
     </div>
-    <div class="col5">
+    <div class="col2">
       <span class="partName">低评数 ></span>
       <br/>
       <span class="partValue">1</span>
@@ -50,32 +56,9 @@ export default {
 .col2 {
   width: 33%;
   height: 50%;
-
-  margin-top: 15px;
   float: left;
   text-align: center;
 }
-.col3 {
-  width: 33%;
-  height: 50%;
-
-  margin-top: 15px;
-  float: left;
-  text-align: center;
-}
-.col4 {
-  width: 33%;
-  height: 50%;
-  float: left;
-  text-align: center;
-}
-.col5 {
-  width: 33%;
-  height: 50%;
-  float: left;
-  text-align: center;
-}
-
 .partName {
   font-size: 12px;
 }
