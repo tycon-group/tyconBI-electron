496759662db5b8d5159157dcec7f6d15262850b9
Leoforpeng <leo.peng@shtycon.com>
2020-10-20 17:41:18 +0800

    实现了日志信息的打印

--- src/renderer/components/children/AnalysisPerson.vue
+++ src/renderer/components/children/AnalysisPerson.vue
@@ -21,19 +21,33 @@ export default {
     return {
       itemEmpID: '',
       worklogsData: [],
+      yearss: '',
+      monthss: '',
     };
   },
   mounted() {
     const vm = this;
-    // 用$on事件来接收参数 , 此处是 工作能力上月数据
+    // 用$on事件来接收参数 , 此处是 默认显示的 工作能力 上月数据
     Bus.$on('itemEmpID', (data) => {
       vm.itemEmpID = data;
-      console.log(this.itemEmpID, '测试00000007'); // 这里取到了被点击的列表所对应的人员的信息链接
-      const url = `https://tyconcps.cn:4399/kpi/worklog/trend/?empID=${this.itemEmpID}`; // 还需要加上当财年条件
+      console.log(this.itemEmpID); // 这里取到了被点击的列表所对应的人员的信息链接
+      // 此处判断月份是否是4月初之前，来确认搜索的年份是否-1
+      const date1 = new Date();
+      const years = date1.getFullYear();
+      const months = date1.getMonth();
+      if (months < 4) {
+        this.yearss = years - 1;
+        this.monthss = months - 1;
+      } else {
+        this.yearss = years;
+        this.monthss = months - 1;
+      }
+      console.log(this.yearss, this.monthss);
+      const url = `https://tyconcps.cn:4399/kpi/worklog/trend/?empID=${this.itemEmpID}&years=${this.yearss}`; // 还需要加上当财年条件
       this.$http.get(url)
         .then((res) => {
           console.log(res.data, '取到所有日志信息数组，现在取上一个月的');
-          this.worklogsData = res.data[res.data.length - 1];
+          this.worklogsData = res.data[this.monthss];
           console.log(this.worklogsData, '取到上一个月日志信息数组');
           // Bus.$emit('defaultCount', this.worklogsData); // 将上月数据数组通过Bus传递
         })
