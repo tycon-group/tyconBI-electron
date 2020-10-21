fca6244925daa4b93ccb717502139c0d3c830148
Leoforpeng <leo.peng@shtycon.com>
2020-10-21 11:06:18 +0800

    替换按钮组内容至选择框

--- src/renderer/components/path/ButtonGroupTime.vue
+++ src/renderer/components/path/ButtonGroupTime.vue
@@ -1,25 +1,53 @@
 <template>
-  <div class="button_group_time">
-    <a-button-group>
-      <a-button>月</a-button>
-      <a-button>季度</a-button>
-      <a-button>半年</a-button>
-      <a-button>财年</a-button>
-    </a-button-group>
+  <div>
+    <a-select
+        show-search
+        placeholder="请选择一个时间跨度~"
+        option-filter-prop="children"
+        style="width: 200px"
+        :filter-option="filterOption"
+        @focus="handleFocus"
+        @blur="handleBlur"
+        @change="handleChange"
+    >
+      <a-icon slot="suffixIcon" type="smile" />
+      <a-select-option v-for="item in dataResources" :key="item.index" :value="item">
+        {{ item }}
+      </a-select-option>
+    </a-select>
   </div>
 </template>
 
 <script>
 export default {
   name: 'ButtonGroupTime',
+  data() {
+    return {
+      dataResources: [
+        '全财年', '上半年', '下半年', '第一季度', '第二季度', '第三季度', '第四季度', '1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月',
+      ],
+    };
+  },
+  methods: {
+    handleChange(value) {
+      console.log(`selected ${value}`);
+    },
+    handleBlur() {
+      console.log('blur');
+    },
+    handleFocus() {
+      console.log('focus');
+    },
+    filterOption(input, option) {
+      return (
+        option.componentOptions.children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
+      );
+    },
+  },
 };
 </script>
 
 <style>
-.button_group_time {
-  float: right;
-  width: 240px;
-  margin-right: 220px;
-}
+
 
 </style>
