4718f31ab0cd43fdd699d7895178d37584a2edbb
Leoforpeng <leo.peng@shtycon.com>
2020-12-28 10:25:48 +0800

    发展潜力模拟数据

--- src/renderer/components/children/Persons/PersonPotential.vue
+++ src/renderer/components/children/Persons/PersonPotential.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="total">
-    <div class="detailed"><a-empty style="margin-top: 10%;"><span slot="description">值为空</span></a-empty></div>
-    <div class="trend"><a-empty style="margin-top: 10%;"><span slot="description">值为空</span></a-empty></div>
+    <div class="potential">
+      <h4 style="margin: 16px 0">
+        技能库：
+      </h4>
+      <div>
+        <a-tag class="tags" color="#f50">
+          office 精通
+        </a-tag>
+        <a-tag class="tags" color="#2db7f5">
+          CAD 良好
+        </a-tag>
+        <a-tag class="tags" color="#87d068">
+          Photoshop 精通
+        </a-tag>
+        <a-tag class="tags" color="#108ee9">
+          AI 良好
+        </a-tag>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PersonWork',
+  name: 'PersonPotential',
 };
 </script>
 
@@ -17,14 +34,17 @@ export default {
   height: 100%;
 }
 
-.detailed {
+.potential {
+  margin-top: 18px;
   width: 100%;
-  height: 40%;
-  border-bottom: 1px solid #dddddd;
+  height: 95%;
 }
 
-.trend {
-  width: 100%;
-  height: 60%;
+.tags {
+  padding-top: 13px;
+  text-align: center;
+  font-size: medium;
+  width: 150px;
+  height: 50px;
 }
 </style>
