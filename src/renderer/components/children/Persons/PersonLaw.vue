64ccb1929e5212a8e50b003a2565c848f223800a
Leoforpeng <leo.peng@shtycon.com>
2020-12-28 09:53:24 +0800

    遵纪守法模拟数据，建议改名

--- src/renderer/components/children/Persons/PersonLaw.vue
+++ src/renderer/components/children/Persons/PersonLaw.vue
@@ -1,14 +1,63 @@
 <template>
   <div class="total">
-    <div class="detailed"><a-empty style="margin-top: 10%;"><span slot="description">值为空</span></a-empty></div>
-    <div class="trend"><a-empty style="margin-top: 10%;"><span slot="description">值为空</span></a-empty></div>
+    <div class="law_table">
+      <a-table
+          :columns="columns"
+          :data-source="datas"
+          @change="onChange"
+          :pagination="{ pageSize: 3 }"
+          :scroll="{ y: 240 }"/>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PersonWork',
+  name: 'PersonLaw',
+  data() {
+    return {
+      datas: [
+        {
+          key: '1',
+          name: '李四',
+          time: '2019-05-10',
+          info: '路上见义勇为，扶摔倒的老奶奶。',
+        },
+        {
+          key: '2',
+          name: '李四',
+          time: '2020-12-10',
+          info: '虚报款项，让公司损失数万元。',
+        },
+      ],
+      columns: [
+        {
+          title: '姓名',
+          dataIndex: 'name',
+        },
+        {
+          title: '时间',
+          dataIndex: 'time',
+          defaultSortOrder: 'descend',
+          // eslint-disable-next-line no-undef
+          sorter: (a, b) => new Date(a.time).getTime() - new Date(b.time).getTime(),
+        },
+        {
+          title: '事件简介',
+          dataIndex: 'info',
+          sorter: (a, b) => a.info.length - b.info.length,
+          sortDirections: ['descend', 'ascend'],
+        },
+      ],
+    };
+  },
+  methods: {
+    onChange(pagination, sorter) {
+      console.log('params', pagination, sorter);
+    },
+  },
 };
+
 </script>
 
 <style>
@@ -17,14 +66,10 @@ export default {
   height: 100%;
 }
 
-.detailed {
+.law_table {
+  margin-top: 18px;
   width: 100%;
-  height: 40%;
-  border-bottom: 1px solid #dddddd;
+  height: 95%;
 }
 
-.trend {
-  width: 100%;
-  height: 60%;
-}
 </style>
